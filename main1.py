7db1054cca68e583ace5e8b72c275b0b3e9e634a
Satyam Singh Chauhan <sat00yam@gmail.com>
2020-06-09 11:23:30 +0530

    update main1.py

--- main1.py
+++ main1.py
@@ -11,17 +11,17 @@ import string
 
 def func():
         r = sr.Recognizer()
-        isl_gif=['all the best', 'any questions', 'are you angry', 'are you busy', 'are you hungry', 'are you sick', 'be careful',
+        isl_gif=['any questions', 'are you angry', 'are you busy', 'are you hungry', 'are you sick', 'be careful',
                 'can we meet tomorrow', 'did you book tickets', 'did you finish homework', 'do you go to office', 'do you have money',
                 'do you want something to drink', 'do you want tea or coffee', 'do you watch TV', 'dont worry', 'flower is beautiful',
                 'good afternoon', 'good evening', 'good morning', 'good night', 'good question', 'had your lunch', 'happy journey',
                 'hello what is your name', 'how many people are there in your family', 'i am a clerk', 'i am bore doing nothing', 
                  'i am fine', 'i am sorry', 'i am thinking', 'i am tired', 'i dont understand anything', 'i go to a theatre', 'i love to shop',
                 'i had to say something but i forgot', 'i have headache', 'i like pink colour', 'i live in nagpur', 'lets go for lunch', 'my mother is a homemaker',
-                'my name is john', 'nice to meet you', 'no smoking please', 'open the door', 'please call an ambulance', 'please call me later',
+                'my name is john', 'nice to meet you', 'no smoking please', 'open the door', 'please call me later',
                 'please clean the room', 'please give me your pen', 'please use dustbin dont throw garbage', 'please wait for sometime', 'shall I help you',
                 'shall we go together tommorow', 'sign language interpreter', 'sit down', 'stand up', 'take care', 'there was traffic jam', 'wait I am thinking',
-                'what are you doing', 'what is the problem', 'what is todays date', 'what is your age', 'what is your father do', 'what is your job',
+                'what are you doing', 'what is the problem', 'what is todays date', 'what is your father do', 'what is your job',
                 'what is your mobile number', 'what is your name', 'whats up', 'when is your interview', 'when we will go', 'where do you stay',
                 'where is the bathroom', 'where is the police station', 'you are wrong','address','agra','ahemdabad', 'all', 'april', 'assam', 'august', 'australia', 'badoda', 'banana', 'banaras', 'banglore',
 'bihar','bihar','bridge','cat', 'chandigarh', 'chennai', 'christmas', 'church', 'clinic', 'coconut', 'crocodile','dasara',
@@ -45,11 +45,10 @@ def func():
                         try:
                                 a=r.recognize_sphinx(audio)
                                 print("you said " + a.lower())
-                                
                                 for c in string.punctuation:
                                     a= a.replace(c,"")
                                     
-                                if(a.lower()=='goodbye'):
+                                if(a.lower()=='goodbye' or a.lower()=='good bye' or a.lower()=='bye'):
                                         print("oops!Time To say good bye")
                                         break
                                 
@@ -110,7 +109,7 @@ def func():
                                                             continue
 
                         except:
-                               print("Could not listen")
+                               print(" ")
                         plt.close()
 
 while 1:
